53c93c67d20b0d48525f1a84396009c8d8cde6dc
Patplays852 <patplays852@gmail.com>
2014-01-15 14:00:42 -0500

    Problem 48 solved

---
+++ ProblemSolver/ProblemSolver/src/Problem048.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace ProblemSolver.src
+{
+    class Problem048 : SolvedProblems
+    {
+        public override void Solve()
+        {
+            BigInteger num = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                num += BigInteger.Pow(i, i);
+            }
+
+            EulerHelper.printAns(num.ToString().Substring((num.ToString().Length - 10), 10).ToString());
+        }
+    }
+}
