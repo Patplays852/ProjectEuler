c4fbb1a2ce99bbaad9927c3a31fb35299d52732d
Patplays852 <patplays852@gmail.com>
2014-01-15 21:56:20 -0500

    40 Solved

---
+++ ProblemSolver/ProblemSolver/src/Problem040.cs
@@ -0,0 +1,45 @@
+﻿/*
+Champernowne's constant
+Problem 40
+An irrational decimal fraction is created by concatenating the positive integers:
+
+0.123456789101112131415161718192021...
+
+It can be seen that the 12th digit of the fractional part is 1.
+
+If dn represents the nth digit of the fractional part, find the value of the following expression.
+
+d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolver.src
+{
+    class Problem040 : SolvedProblems
+    {
+        public override void Solve()
+        {
+            string str = "";
+            int i = 1;
+            do{
+                str += i.ToString();
+                i++;
+            }while(str.Length < 1000000);
+
+            int prod = Convert.ToInt32(str.Substring(0, 1)) *
+                Convert.ToInt32(str.Substring(9, 1)) *
+                Convert.ToInt32(str.Substring(99, 1)) *
+                Convert.ToInt32(str.Substring(999, 1)) *
+                Convert.ToInt32(str.Substring(9999, 1)) *
+                Convert.ToInt32(str.Substring(99999, 1)) *
+                Convert.ToInt32(str.Substring(999999, 1));
+
+            EulerHelper.printAns(prod.ToString());
+
+
+        }
+    }
+}
